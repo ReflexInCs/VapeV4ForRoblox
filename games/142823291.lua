827976e9f1e585aaf28b193b71cbbf9e9ffa607a
ReflexInCs <maxime.mirouze@icloud.com>
2025-10-18 13:28:58 -0200

    Add files via upload

---
+++ games/142823291.lua
@@ -0,0 +1,144 @@
+local loadstring = function(...)
+	local res, err = loadstring(...)
+	if err and vape then
+		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+	end
+	return res
+end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return areadfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/ReflexInCs/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
+local run = function(func)
+	func()
+end
+local cloneref = cloneref or function(obj)
+	return obj
+end
+
+local playersService = cloneref(game:GetService('Players'))
+local runService = cloneref(game:GetService('RunService'))
+local lplr = playersService.LocalPlayer
+
+local vape = shared.vape
+local entitylib = loadstring(downloadFile('newvape/libraries/entity.lua'), 'entitylibrary')()
+
+local function notif(...)
+	return vape:CreateNotification(...)
+end
+
+entitylib.start()
+
+-- Shoot Murderer Module
+run(function()
+	local ShootMurderer
+	local shootOffset = 2.8
+	local offsetToPingMult = 1
+	
+	local function getHRP(plr)
+		return plr.Character and (plr.Character:FindFirstChild("HumanoidRootPart") or plr.Character:FindFirstChild("UpperTorso"))
+	end
+	
+	local function getHumanoid(plr)
+		return plr.Character and plr.Character:FindFirstChild("Humanoid")
+	end
+	
+	local function getMurderer()
+		for _, plr in ipairs(playersService:GetPlayers()) do
+			if plr ~= lplr and (plr.Backpack:FindFirstChild("Knife") or (plr.Character and plr.Character:FindFirstChild("Knife"))) then
+				return plr
+			end
+		end
+	end
+	
+	local function getPredictedPosition(target)
+		local hrp = getHRP(target)
+		local hum = getHumanoid(target)
+		if not hrp or not hum then return Vector3.zero end
+		
+		local velocity = hrp.AssemblyLinearVelocity
+		local moveDir = hum.MoveDirection
+		local predicted = hrp.Position + (velocity * Vector3.new(0, 0.5, 0)) * (shootOffset / 15) + moveDir * shootOffset
+		predicted *= (((lplr:GetNetworkPing() * 1000) * ((offsetToPingMult - 1) * 0.01)) + 1)
+		return predicted
+	end
+	
+	local function shootMurderer()
+		local hasGun = lplr.Backpack:FindFirstChild("Gun") or (lplr.Character and lplr.Character:FindFirstChild("Gun"))
+		if not hasGun then
+			notif('Shoot Murderer', 'You are not the sheriff/hero or don\'t have a gun.', 3, 'warning')
+			return false
+		end
+		
+		local murderer = getMurderer()
+		if not murderer then
+			notif('Shoot Murderer', 'No murderer found.', 3, 'warning')
+			return false
+		end
+		
+		-- Equip Gun
+		if lplr.Backpack:FindFirstChild("Gun") then
+			local hum = getHumanoid(lplr)
+			if hum then hum:EquipTool(lplr.Backpack:FindFirstChild("Gun")) end
+		end
+		
+		local predicted = getPredictedPosition(murderer)
+		
+		local args = {
+			[1] = 1,
+			[2] = predicted,
+			[3] = "AH2"
+		}
+		
+		local gunScript = lplr.Character and lplr.Character:FindFirstChild("Gun") and lplr.Character.Gun:FindFirstChild("KnifeLocal")
+		if gunScript and gunScript:FindFirstChild("CreateBeam") then
+			gunScript.CreateBeam.RemoteFunction:InvokeServer(unpack(args))
+			notif('Shoot Murderer', 'Shot at murderer!', 2)
+			return true
+		else
+			notif('Shoot Murderer', 'Gun script missing CreateBeam.', 3, 'error')
+			return false
+		end
+	end
+	
+	ShootMurderer = vape.Categories.Misc:CreateModule({
+		Name = 'ShootMurderer',
+		Function = function(callback)
+			if callback then
+				-- Shoot immediately when toggled on
+				local success = shootMurderer()
+				
+				if success then
+					-- Auto-disable after shooting
+					task.wait(0.5)
+					if ShootMurderer.Enabled then
+						ShootMurderer:Toggle()
+					end
+				else
+					-- If failed, disable immediately
+					if ShootMurderer.Enabled then
+						ShootMurderer:Toggle()
+					end
+				end
+			end
+		end,
+		Tooltip = 'Automatically shoots the murderer when toggled (Sheriff/Hero only)'
+	})
+end)
